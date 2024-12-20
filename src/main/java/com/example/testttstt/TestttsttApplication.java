f5e5e6934e1b1d7de3bc10407ac3a9e79e0e40dc
agyassir <aginaneyassir@gmail.com>
2024-12-20 08:14:05 +0100

    the test of the test

---
+++ src/main/java/com/example/testttstt/TestttsttApplication.java
@@ -0,0 +1,13 @@
+package com.example.testttstt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestttsttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestttsttApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testttstt/TestttsttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testttstt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestttsttApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
